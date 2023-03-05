d90303b88f15908390910246fbc3c12c60f591dd
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-03-05 18:17:20 +0100

    feat: created dashboard i18n

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -6,6 +6,7 @@ import { DashboardComponent } from './dashboard.component';
 import { NavbarModule } from '../../components/navbar/navbar.module';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [DashboardComponent, ProjectCardComponent],
@@ -14,6 +15,7 @@ import { ProjectCardComponent } from './components/project-card/project-card.com
     DashboardRoutingModule,
     NavbarModule,
     ScrollingModule,
+    TranslateModule,
   ],
 })
 export class DashboardModule {}
