d6fe6f0b6441faee57351cf807e1f40b1d0689e9
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-03-07 23:15:41 +0100

    feat: list routes

--- src/app/pages/projects/pages/routes/routes.component.ts
+++ src/app/pages/projects/pages/routes/routes.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { RoutesService } from '../../../../services/routes.service';
+import { RouteInterface } from '../../../../interfaces/route.interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-routes',
   templateUrl: './routes.component.html',
-  styleUrls: ['./routes.component.scss']
+  styleUrls: ['./routes.component.scss'],
 })
 export class RoutesComponent implements OnInit {
+  routes?: RouteInterface[];
+  projectId?: number;
 
-  constructor() { }
+  constructor(
+    private routesService: RoutesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params) => {
+      this.projectId = params['id'];
+      this.#getRoutes();
+    });
   }
 
+  #getRoutes() {
+    if (this.projectId === undefined) return;
+    this.routesService.getRoutes(this.projectId).subscribe((routes) => {
+      this.routes = routes.data;
+    });
+  }
 }
--- src/app/pages/projects/pages/routes/routes.module.ts
+++ src/app/pages/projects/pages/routes/routes.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { RoutesRoutingModule } from './routes-routing.module';
 import { RoutesComponent } from './routes.component';
-
+import { NavbarModule } from '../../../../components/navbar/navbar.module';
+import { RouteListItemComponent } from './components/route-list-item/route-list-item.component';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
-  declarations: [
-    RoutesComponent
-  ],
-  imports: [
-    CommonModule,
-    RoutesRoutingModule
-  ]
+  declarations: [RoutesComponent, RouteListItemComponent],
+  imports: [CommonModule, RoutesRoutingModule, NavbarModule, MatChipsModule],
 })
-export class RoutesModule { }
+export class RoutesModule {}
