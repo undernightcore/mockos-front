bb9e54f5ff880678cd3acdeb1935b24cf862d034
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-03-08 23:11:09 +0100

    feat: get and edit route details

--- src/app/pages/projects/pages/routes/routes.component.ts
+++ src/app/pages/projects/pages/routes/routes.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { RoutesService } from '../../../../services/routes.service';
 import { RouteInterface } from '../../../../interfaces/route.interface';
 import { ActivatedRoute } from '@angular/router';
+import { openToast } from '../../../../utils/toast.utils';
+import { FormControl, FormGroup } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-routes',
@@ -10,11 +13,13 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RoutesComponent implements OnInit {
   routes?: RouteInterface[];
+  selectedRoute?: FormGroup;
   projectId?: number;
 
   constructor(
     private routesService: RoutesService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private translateService: TranslateService
   ) {}
 
   ngOnInit() {
@@ -22,6 +27,42 @@ export class RoutesComponent implements OnInit {
       this.projectId = params['id'];
       this.#getRoutes();
     });
+  }
+
+  selectRoute(route: RouteInterface) {
+    this.selectedRoute = new FormGroup({
+      id: new FormControl(route.id),
+      name: new FormControl(route.name),
+      method: new FormControl(route.method),
+      endpoint: new FormControl(route.endpoint),
+      enabled: new FormControl(route.enabled),
+      created_at: new FormControl(route.created_at),
+      updated_at: new FormControl(route.updated_at),
+    });
+  }
+
+  updateRoute() {
+    if (!this.selectedRoute || this.selectedRoute.invalid) return;
+    this.routesService
+      .editRoute(this.selectedRoute.value.id, this.selectedRoute.value)
+      .subscribe({
+        next: (newRoute) => {
+          this.selectedRoute?.setValue(newRoute);
+          openToast(
+            this.translateService.instant('PAGES.ROUTES.UPDATED_SUCCESSFULLY', {
+              route: newRoute.name,
+            }),
+            'success'
+          );
+          this.#getRoutes();
+        },
+        error: () => {
+          const oldRoute = this.routes?.find(
+            (route) => route.id === this.selectedRoute?.value.id
+          );
+          if (oldRoute) this.selectedRoute?.setValue(oldRoute);
+        },
+      });
   }
 
   #getRoutes() {
--- src/app/pages/projects/pages/routes/routes.module.ts
+++ src/app/pages/projects/pages/routes/routes.module.ts
@@ -6,9 +6,27 @@ import { RoutesComponent } from './routes.component';
 import { NavbarModule } from '../../../../components/navbar/navbar.module';
 import { RouteListItemComponent } from './components/route-list-item/route-list-item.component';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpChipModule } from '../../../../components/http-chip/http-chip.module';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [RoutesComponent, RouteListItemComponent],
-  imports: [CommonModule, RoutesRoutingModule, NavbarModule, MatChipsModule],
+  imports: [
+    CommonModule,
+    RoutesRoutingModule,
+    NavbarModule,
+    MatChipsModule,
+    MatInputModule,
+    FormsModule,
+    HttpChipModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    TranslateModule,
+  ],
 })
 export class RoutesModule {}
