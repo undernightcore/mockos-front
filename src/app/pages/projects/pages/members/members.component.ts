5d7d2f4ef9fbc62b016a77d347683628a95868ae
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-03-13 20:44:42 +0100

    feat: create member card and list

--- src/app/pages/projects/pages/members/members.component.ts
+++ src/app/pages/projects/pages/members/members.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../../../../services/project.service';
 import { ActivatedRoute } from '@angular/router';
-import { forkJoin } from 'rxjs';
+import { finalize } from 'rxjs';
 import { ProjectInterface } from '../../../../interfaces/project.interface';
+import { UserInterface } from '../../../../interfaces/user.interface';
 
 @Component({
   selector: 'app-members',
@@ -12,6 +13,9 @@ import { ProjectInterface } from '../../../../interfaces/project.interface';
 export class MembersComponent implements OnInit {
   projectId?: number;
   project?: ProjectInterface;
+  members?: UserInterface[];
+  maxMembers = 0;
+  #isFetching = false;
 
   constructor(
     private projectService: ProjectService,
@@ -21,16 +25,43 @@ export class MembersComponent implements OnInit {
   ngOnInit() {
     this.activatedRoute.params.subscribe((params) => {
       this.projectId = params['id'];
+      this.#getProject();
     });
-    this.#getProject();
+  }
+
+  handleScroll(event: Event) {
+    if (!this.members) return;
+    const { scrollTop, scrollHeight, offsetHeight } =
+      event.target as HTMLElement;
+    if (
+      scrollHeight - (scrollTop + offsetHeight) > 200 ||
+      this.members.length >= this.maxMembers
+    )
+      return;
+    const pageToRequest = this.members.length / 20 + 1;
+    this.#getMemberList(pageToRequest);
   }
 
   #getProject() {
     if (this.projectId === undefined) return;
-    forkJoin([this.projectService.getProject(this.projectId)]).subscribe(
-      ([project]) => {
-        this.project = project;
-      }
-    );
+    this.projectService.getProject(this.projectId).subscribe((project) => {
+      this.project = project;
+      this.#getMemberList(1);
+    });
+  }
+
+  #getMemberList(page: number) {
+    if (this.#isFetching || this.projectId === undefined) return;
+    this.#isFetching = true;
+    this.projectService
+      .getMemberList(this.projectId, page, 20)
+      .pipe(finalize(() => (this.#isFetching = false)))
+      .subscribe((members) => {
+        this.members =
+          page === 1
+            ? members.data
+            : [...(this.members ?? []), ...members.data];
+        this.maxMembers = members.meta.total;
+      });
   }
 }
--- src/app/pages/projects/pages/members/members.module.ts
+++ src/app/pages/projects/pages/members/members.module.ts
@@ -5,9 +5,10 @@ import { MembersRoutingModule } from './members-routing.module';
 import { MembersComponent } from './members.component';
 import { NavbarModule } from '../../../../components/navbar/navbar.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { MemberCardComponent } from './components/member-card/member-card.component';
 
 @NgModule({
-  declarations: [MembersComponent],
+  declarations: [MembersComponent, MemberCardComponent],
   imports: [CommonModule, MembersRoutingModule, NavbarModule, TranslateModule],
 })
 export class MembersModule {}
