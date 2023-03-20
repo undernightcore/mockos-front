cb51f4581f995006454cfe93b43b59ee20be41db
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-03-20 20:21:53 +0100

    feat: created invitation badge

--- src/app/pages/projects/projects.module.ts
+++ src/app/pages/projects/projects.module.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatBadgeModule,
   ],
 })
 export class ProjectsModule {}
