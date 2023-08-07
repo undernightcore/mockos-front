537038f740c4d09d05d5379a2c5d0ca3612bac13
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-08-07 17:16:30 +0200

    feat: create tokens service

---
+++ src/app/interfaces/tokens.interface.ts
@@ -0,0 +1,7 @@
+export interface TokensInterface {
+  id: number;
+  token: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
---
+++ src/app/services/tokens/tokens.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EnvService } from '../env/env.service';
+import { PaginatedResponseInterface } from '../../interfaces/paginated-response.interface';
+import { TokensInterface } from '../../interfaces/tokens.interface';
+import { MessageInterface } from '../../interfaces/message.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokensService {
+  constructor(private httpClient: HttpClient, private envService: EnvService) {}
+
+  getTokens(projectId: number, page = 1, perPage = 10) {
+    return this.httpClient.get<PaginatedResponseInterface<TokensInterface>>(
+      `${this.envService.getEnv('apiUrl')}/projects/${projectId}/tokens`,
+      { params: { page, perPage } }
+    );
+  }
+
+  createToken(projectId: number, name: string) {
+    return this.httpClient.post<TokensInterface>(
+      `${this.envService.getEnv('apiUrl')}/projects/${projectId}/tokens`,
+      { name }
+    );
+  }
+
+  deleteToken(tokenId: number) {
+    return this.httpClient.get<MessageInterface>(
+      `${this.envService.getEnv('apiUrl')}/tokens/${tokenId}`
+    );
+  }
+}
