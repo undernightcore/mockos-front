6efbd233e576caeb9f019dc7d94ca2c65deb5d96
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-10-03 19:01:37 +0200

    feat: allow to select language

--- src/app/pages/projects/pages/contracts/contracts.component.ts
+++ src/app/pages/projects/pages/contracts/contracts.component.ts
@@ -1,16 +1,12 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import SwaggerUI from 'swagger-ui';
 import { ContractsService } from '../../../../services/contracts/contracts.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { load } from 'js-yaml';
 import { MinimalContractInterface } from '../../../../interfaces/contract.interface';
+import { debounceTime } from 'rxjs';
+import {isValidJson} from "../../../../utils/string.utils";
 
 @Component({
   selector: 'app-contracts',
@@ -23,6 +19,9 @@ export class ContractsComponent implements AfterViewInit {
 
   swagger?: SwaggerUI;
   contract = new FormControl('');
+
+  selectedLang = 'yaml';
+  languages = ['yaml', 'json'];
 
   parsedLocal?: MinimalContractInterface;
   parsedRemote?: MinimalContractInterface;
@@ -40,13 +39,15 @@ export class ContractsComponent implements AfterViewInit {
   }
 
   updateContract() {
-    this.contractsService.updateContract(
-      this.projectId,
-      this.parsedRemote?.info?.version ?? null,
-      this.contract.value ?? ''
-    ).subscribe(() => {
-      this.#getContract()
-    });
+    this.contractsService
+      .updateContract(
+        this.projectId,
+        this.parsedRemote?.info?.version ?? null,
+        this.contract.value ?? ''
+      )
+      .subscribe(() => {
+        this.#getContract();
+      });
   }
 
   #getContract() {
@@ -54,12 +55,15 @@ export class ContractsComponent implements AfterViewInit {
       this.parsedRemote = contract
         ? this.#parseContract(contract?.swagger)
         : null;
+
+      this.selectedLang = contract && isValidJson(contract.swagger) ? 'json' : 'yaml'
+
       this.contract.setValue(contract?.swagger ?? '');
     });
   }
 
   #handleContractChange() {
-    this.contract.valueChanges.subscribe(() => {
+    this.contract.valueChanges.pipe(debounceTime(100)).subscribe(() => {
       this.parsedLocal = this.#parseContract(String(this.contract.value));
       this.areSameContracts = this.#calculateIfSameContracts();
 
@@ -72,7 +76,7 @@ export class ContractsComponent implements AfterViewInit {
 
     SwaggerUI({
       domNode: this.swaggerElement.nativeElement,
-      spec: this.parsedLocal
+      spec: this.parsedLocal,
     });
   }
 
@@ -82,7 +86,8 @@ export class ContractsComponent implements AfterViewInit {
     } catch {
       try {
         return load(value);
-      } catch {
+      } catch (e) {
+        console.log(e);
         return {};
       }
     }
--- src/app/pages/projects/pages/contracts/contracts.module.ts
+++ src/app/pages/projects/pages/contracts/contracts.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ContractsRoutingModule } from './contracts-routing.module';
 import { ContractsComponent } from './contracts.component';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [ContractsComponent],
@@ -17,6 +19,9 @@ import { MatIconModule } from '@angular/material/icon';
     ReactiveFormsModule,
     MatButtonModule,
     MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
   ],
 })
 export class ContractsModule {}
